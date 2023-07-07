78c00db13d9ce9a41c65c73f2f4a83531f51ab3f
Vitor Magno <vmcg@ic.ufal.br>
2023-07-07 12:41:47 -0300

    style: some new lines added

--- atvNumpy.py
+++ atvNumpy.py
@@ -5,32 +5,39 @@ fullArray = np.array([1,2,3,4])
 emptyArray = np.empty(4)
 print(fullArray)
 print(emptyArray)
+
 # %%
 # array filled with zero
 zeros = np.zeros(4)
 print(zeros)
+
 # %%
 # filled with one
 one = np.ones(4)
 print(one)
+
 # %%
 # checking for a specific row
 matrix = np.array([[1,2,3,4], [5,6,7,8]])
 print(matrix[1,:])
+
 # %%
 # removing rows containing non-numeric values
 nonnumeric = np.array([[14,np.nan,np.nan],[1,2,3]])
 nonnumeric = nonnumeric[~np.isnan(nonnumeric).any(axis=1)]
 nonnumeric
+
 # %%
 # removing single dimensional entries
 nonnumeric = nonnumeric[0,:]
 print(nonnumeric)
+
 # %%
 # finding sequences
 a = np.array([1234, 1542, 1234, 1567])
 count = (a==1234).sum()
 print(count)
+
 # %%
 # most frequent value in a np.array
 arr = np.array([2, 3, 4, 5, 3, 4, 5, 3, 5, 4, 7, 8, 3, 6, 2])
@@ -42,18 +49,20 @@ def mostFrequent(arr):
 
 result = mostFrequent(arr)
 print(result)
+
 # %%
 # combining one and two dimensional arrays
 arr1 = np.array([1,2,3,4])
 arr2 = np.array([[5,6,7,8], [9,10,11,12]])
 arr3 = np.vstack((arr1,arr2))
 print(arr3)
-# %%
-# creating an array of all combinations of another two arrays (I don't know)
 
+# %%
+# creating an array of all combinations(2,2) of another two arrays
 result = np.meshgrid(arr1[:3],arr2[0,:3])
-result = np.array(result).T
+result = np.array(result).T.reshape(-1,2)
 print(result)
+
 # %%
 # making a border around an array
 modes = ['edge','linear_ramp','maximum','mean','median','minimum','reflect','symmetric', 'wrap','empty','constant']
@@ -61,16 +70,19 @@ for i in modes:
     arrBorder = np.pad(arr3, pad_width=1, mode=i)
     print(i)
     print(arrBorder)
+
 # %%
 # comparing two numpy arrays
 result = np.array_equal(arr1,arr2)
 print(result)
+
 # %%
 # checking for a specified value
 value = 1
 result = np.isin(arr1, value) # np.array 
 result2 = (arr1 == 1) 
 print(result, result2)
+
 # %%
 # get all 2d diagonals of a 3d numppy array
 array = np.array([[[1, 2, 3], [4, 5, 6],[7, 8, 9]],[[10, 11, 12],[13, 14, 15],[16, 17, 18]],[[19, 20, 21],[22, 23, 24],[25, 26, 27]]])
@@ -79,3 +91,9 @@ for i in range(array.shape[0]):
     diagonals.append(np.diagonal(array[i]))
 diagonals = np.array(diagonals)
 print(diagonals)
+
+# %%
+# flattening a matrix 
+array.flatten()
+
+# %%

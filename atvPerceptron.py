08b7678d38a9b6dd669dab15463a91607d8cb542
Vitor Magno <vmcg@ic.ufal.br>
2023-07-13 08:42:11 -0300

    style: code dependency organization

--- atvPerceptron.py
+++ atvPerceptron.py
@@ -1,6 +1,6 @@
 # %%
 import numpy as np
-
+from perceptron import Perceptron
 # %%
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
@@ -9,39 +9,6 @@ X, y = make_blobs(n_samples=150,n_features=2,centers=2, cluster_std=1.05, random
 X_train, X_test, y_train, y_test = train_test_split(X,y, test_size=0.2, random_state=123)
 plt.scatter(X[:,0], X[:,1], c=y)
 plt.show()
-
-# %%
-class Perceptron:
-    def __init__(self, learning_rate=0.01, i=100):
-        self.lr = learning_rate
-        self.iterations = i
-        self.activation_func = self._activation_
-        self.weights = None
-        self.bias = None
-
-    def fit(self, X, y):
-        n_samples, n_features = X.shape
-        # init weights
-        self.weights = np.zeros(n_features)
-        self.bias = 0
-        y_ = np.array([1 if i>0 else 0 for i in y])
-
-        for elem in range(self.iterations):
-            for idx, x_ in enumerate(X):
-                linear_output = np.dot(x_, self.weights) + self.bias
-                y_predicted = self.activation_func(linear_output)
-
-                update = self.lr * (y_[idx]-y_predicted)
-                self.weights += update * x_
-                self.bias += update
-
-    def predict(self, X):
-        linear_output = np.dot(X, self.weights) + self.bias
-        y_predicted = self.activation_func(linear_output)
-        return y_predicted
-
-    def _activation_(self, x):
-        return np.where(x>=0, 1,0)
 
 # %%
 alfa = 0.01
--- perceptron.py
+++ perceptron.py
@@ -1,52 +1,32 @@
-#%%
 import numpy as np
-import matplotlib.pyplot as plt
-from sklearn.datasets import make_blobs
-from mlxtend.plotting import plot_decision_regions
-# %%
-np.random.seed(1)
-X, y = make_blobs(centers=2, cluster_std=1, random_state=1)
-weights = np.random.rand(3)
-plt.scatter(X[:,0], X[:,1], c=y)
-plt.show()
-
-# %%
-class Perceptron(object):
-    def __init__(self, learning_rate=0.01, epochs=50, verbose=False):
-        self.verbose = verbose
-        self.learning_rate = learning_rate
-        self.epochs = epochs
-
-    def _sum(self, X):
-        return np.dot(X, self._weights) + self._bias
-
-    def _activation(self, m):
-        return np.where(m>=0.0, 1,-1)
+class Perceptron:
+    def __init__(self, learning_rate=0.01, i=100):
+        self.lr = learning_rate
+        self.iterations = i
+        self.activation_func = self._activation_
+        self.weights = None
+        self.bias = None
 
     def fit(self, X, y):
-        self._bias = np.random.uniform(-1,1)
-        self._weights = np.random.uniform(-1,1,(X.shape[1]))
-        self._errors = []
-        for epoch in range(self.epochs):
-            erros = 0
-            for x, target in zip(X,y):
-                output = self.predict(x)
-                update = self.learning_rate * (target-output)
-                self._bias += update
-                self._weights += update * x
-                erros += int(update!=0.0)
-            self._errors.append(erros)
-        return self
-    def predict(self,X):
-        return self._activation(self._sum(X))
+        n_samples, n_features = X.shape
+        # init weights
+        self.weights = np.zeros(n_features)
+        self.bias = 0
+        y_ = np.array([1 if i>0 else 0 for i in y])
 
-# %%
-alfa = 0.01
-epoch = 60
-# %%
-percp = Perceptron(epochs = epoch, learning_rate=alfa)
-percp.fit(X,y)
-plot_decision_regions(X,y,clf=percp)
-plt.title('Perceptron')
-plt.show()
-# %%
+        for elem in range(self.iterations):
+            for idx, x_ in enumerate(X):
+                linear_output = np.dot(x_, self.weights) + self.bias
+                y_predicted = self.activation_func(linear_output)
+
+                update = self.lr * (y_[idx]-y_predicted)
+                self.weights += update * x_
+                self.bias += update
+
+    def predict(self, X):
+        linear_output = np.dot(X, self.weights) + self.bias
+        y_predicted = self.activation_func(linear_output)
+        return y_predicted
+
+    def _activation_(self, x):
+        return np.where(x>=0, 1,0)
